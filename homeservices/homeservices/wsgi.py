1a360b28f974cd32733d892429485ed99a3f021c
rajuquri <165357927+rajuquri@users.noreply.github.com>
2026-06-07 18:11:31 +0530

    Auto-run migrations on Vercel

--- homeservices/homeservices/wsgi.py
+++ homeservices/homeservices/wsgi.py
@@ -1,4 +1,10 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homeservices.settings')
+if os.getenv('VERCEL') or os.getenv('VERCEL_ENV'):
+    django.setup()
+    call_command('migrate', interactive=False, verbosity=0)
 application = get_wsgi_application()
